41f26bf4643decd950570414b021374b157c5b26
happy061421@navar.com <you@example.com>
2021-07-28 11:24:47 +0900

    second week

---
+++ 3.recursion+condition/for_recursion.py
@@ -0,0 +1,28 @@
+# for문 무작정 따라해보기
+for i in range(1, 11):
+    if (i%2 == 0):
+      print(i, "은/는 짝수입니다.")
+    else:
+      print(i, "은/는 홀수입니다.")
+
+# for문의 구조
+# for i in 범위:
+# ...반복할 명령어1
+# ...반복할 명령어2
+
+# for문 with list
+mylist = ['해달이', '사스미', '메기']
+for i in mylist:
+    print(i)
+print("반복끝") 
+
+# print list with range
+print(list(range(10))) # 시작점 기본값은 0
+print(list(range(1,11)))
+print(list(range(1,20,3)))
+print(list(range(20,0,-3)))
+
+# for문 with range
+for i  in range(1,11):
+    print(i, end=" ")
+print('반복끝')    
---
+++ 3.recursion+condition/procedure.py
@@ -0,0 +1,14 @@
+#숫자형 자료
+#정수형
+a = 154
+print(type(a))
+a = 0
+print(type(a))
+a = -25
+print(type(a))
+
+#실수형
+b = 181.34
+print(type(b))
+b = -22.22
+print(type(b))
